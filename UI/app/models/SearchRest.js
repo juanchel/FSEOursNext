6859fe1a351f1273762af4f5948c8e0d0d5b2651
cici37 <cici.huang@west.cmu.edu>
2014-10-26 19:13:04 -0700

    [UI] Debug stopwords.

--- UI/app/models/SearchRest.js
+++ UI/app/models/SearchRest.js
@@ -42,13 +42,14 @@ SearchQuery.prototype.send = function(callback) {
   }
   
   var stopwords = ['a','able','about','across','after','all','almost','also','am','among','an','and','any','are','as','at','be','because','been','but','by','can','cannot','could','dear','did','do','does','either','else','ever','every','for','from','get','got','had','has','have','he','her','hers','him','his','how','however','i','if','in','into','is','it','its','just','least','let','like','likely','may','me','might','most','must','my','neither','no','nor','not','of','off','often','on','only','or','other','our','own','rather','said','say','says','she','should','since','so','some','than','that','the','their','them','then','there','these','they','this','tis','to','too','twas','us','wants','was','we','were','what','when','where','which','while','who','whom','why','will','with','would','yet','you','your'];
-  var spllittedKeyword = this.keywords.split(" ");
-  for(var i =0; i <= spllittedKeyword.length; i++){
-    if(stopwords.indexOf(spllittedKeyword[i]) >= 0){
-      spllittedKeyword.splice(stopwords.indexOf(spllittedKeyword[i]),1," ");
+  var splittedKeyword = this.keywords.split(" ");
+  console.log("The spllittedKeyword is: " + splittedKeyword);
+  for(var i =0; i <= splittedKeyword.length; i++){
+    if(stopwords.indexOf(splittedKeyword[i]) >= 0){
+      splittedKeyword.splice(splittedKeyword.indexOf(splittedKeyword[i]),1);
     }
   }
-  var newKeyword = spllittedKeyword.join(" ");
+  var newKeyword = splittedKeyword.join(" ");
   
   var options = {
     url : url,
