5d4e4de84dcc4463a6071f5ed3f5c0455970309e
cici37 <cici.huang@west.cmu.edu>
2014-10-26 18:26:24 -0700

    [UI]Add stopwords before searching.

--- UI/app/models/SearchRest.js
+++ UI/app/models/SearchRest.js
@@ -40,15 +40,25 @@ SearchQuery.prototype.send = function(callback) {
     console.warn("SearchQuery aborted, unknown type.");
     callback("unknown SearchQuery type");
   }
+  
+  var stopwords = ['a','able','about','across','after','all','almost','also','am','among','an','and','any','are','as','at','be','because','been','but','by','can','cannot','could','dear','did','do','does','either','else','ever','every','for','from','get','got','had','has','have','he','her','hers','him','his','how','however','i','if','in','into','is','it','its','just','least','let','like','likely','may','me','might','most','must','my','neither','no','nor','not','of','off','often','on','only','or','other','our','own','rather','said','say','says','she','should','since','so','some','than','that','the','their','them','then','there','these','they','this','tis','to','too','twas','us','wants','was','we','were','what','when','where','which','while','who','whom','why','will','with','would','yet','you','your'];
+  var spllittedKeyword = this.keywords.split(" ");
+  for(var i =0; i <= spllittedKeyword.length; i++){
+    if(stopwords.indexOf(spllittedKeyword[i]) >= 0){
+      spllittedKeyword.splice(stopwords.indexOf(spllittedKeyword[i]),1," ");
+    }
+  }
+  var newKeyword = spllittedKeyword.join(" ");
+  
   var options = {
     url : url,
     body : {
-      'content' : this.keywords
+      'content' : newKeyword
     },
     json : true
   };  
   
-  console.info("send searchForUsername options: " + JSON.stringify(options));
+  console.info("send search options: " + JSON.stringify(options));
 
   request.post(options, function(error, response, body) {
     if (error) {
